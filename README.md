019663c5a9844f0df7911e87a57cac6151704302
Stephen Sloan <steve@stevesloan.com>
2013-01-17 15:03:50 -0800

    First commit after creating project with Lein

---
+++ README.md
@@ -0,0 +1,13 @@
+# caves-of-clojure
+
+A Clojure library designed to ... well, that part is up to you.
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2013 Rafter, Inc.
+
+All rights reserved
---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject caves-of-clojure "0.1.0-SNAPSHOT"
+  :description "Caves of Clojure"
+  :url "https://github.com/bkr/caves-of-clojureE"
+  :dependencies [[org.clojure/clojure "1.4.0"]])
