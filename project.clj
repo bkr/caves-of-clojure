4f0c47e608387c4176683a6ee7ad4b671f6166d2
Stephen Sloan <steve@stevesloan.com>
2013-01-18 12:34:57 -0800

    Updated project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,9 @@
 (defproject caves-of-clojure "0.1.0-SNAPSHOT"
   :description "Caves of Clojure"
-  :url "https://github.com/bkr/caves-of-clojureE"
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :url "https://github.com/bkr/caves-of-clojure"
+  :license {:name "MIT/X11"}
+  :dependencies [
+    [org.clojure/clojure "1.4.0"]
+    [clojure-lanterna "0.9.0"]
+  ]
+)
