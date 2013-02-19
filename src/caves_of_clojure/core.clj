8b23fcfc945dd906340bdfc88e960494021592f8
Stephen Sloan <steve@stevesloan.com>
2013-02-19 12:15:30 -0800

    Working on part2

--- src/caves_of_clojure/core.clj
+++ src/caves_of_clojure/core.clj
@@ -1,22 +1,63 @@
 (ns caves-of-clojure.core
   (:require [lanterna.screen :as lanterna-screen]))
 
+(defrecord UI [kind])
+(defrecord World [])
+(defrecord Game [world uis input])
+
+(defn clear-screen [screen]
+  (let [blank (apply str (repeat 80 \space))]
+    (doseq [row (range 24)]
+      (lanterna-screen/put-string screen 0 row blank))))
+
+(defmulti draw-ui
+  (fn [ui game screen]
+    (:kind ui)))
+
+(defmethod draw-ui :start [ui game screen]
+  (lanterna-screen/put-string screen 0 0 "Welcome to the Caves of Clojure!")
+  (lanterna-screen/put-string screen 0 1 "Press enter to win, anything else to lose."))
+
+(defmethod draw-ui :win [ui game screen]
+  (lanterna-screen/put-string screen 0 0 "Congratulations, you win!")
+  (lanterna-screen/put-string screen 0 1 "Press escape to exit, anything else to restart."))
+
+(defmethod draw-ui :lose [ui game screen]
+  (lanterna-screen/put-string screen 0 0 "Sorry, better luck next time.")
+  (lanterna-screen/put-string screen 0 1 "Press escape to exit, anything else to go."))
+
+(defn draw-game [game screen]
+  (clear-screen screen)
+  (doseq [ui (:uis game)]
+    (draw-ui ui game screen))
+  (lanterna-screen/redraw screen))
+
+(defmulti process-input
+  (fn [game input]
+    (:kind (last (:uis game)))))
+
+(defmethod process-input :start [game input]
+  (if (= input :enter)
+    (assoc game :uis [(new UI :win)])
+    (assoc game :uis [(new UI :lose)])))
+
+(defmethod process-input :win [game input]
+  (if (= input :escape)
+    (assoc game :uis [])
+    (assoc game :uis [(new UI :start)])))
+
+(defmethod process-input :lose [game input]
+  (if (= input :escape)
+    (assoc game :uis [])
+    (assoc game :uis [(new UI :start)])))
+
+(defn get-input [game screen]
+  (assoc game :input (lanterna-screen/get-key-blocking screen)))
+
+
+
 ; This is the run function
-(defn run [screen-type]
-  (let [my-screen (lanterna-screen/get-screen screen-type)]
-    (lanterna-screen/in-screen
-      my-screen
-      (doto my-screen
-        (lanterna-screen/put-string 0 0 "Welcome to the Caves of Clojure!")
-        (lanterna-screen/put-string 0 1 "Press any key to exit...")
-        (lanterna-screen/redraw)
-        (lanterna-screen/get-key-blocking)))))
+(defn run [screen-type])
 
 ; Ths is -main
-(defn -main [& args]
-  (let [set-args (set args)
-        screen-type (cond
-                      (set-args ":swing") :swing
-                      (set-args ":text") :text
-                      :else :auto)]
-    (run screen-type)))
+(defn -main [& args])
